30f2a836b22b25fc42361ce14f92931d43dd906f
Alex Li <alelia40@gmail.com>
2019-10-24 19:37:57 -0400

    hello

---
+++ Q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void){
+	int a = 100;
+	int edx;
+	int ecx;
+	
+	while(a < 999){
+		ecx = a;
+		edx = 1374389535;
+		eax = ecx;
+		eax = eax * edx;
+		edx = edx / 32;
+		
+		eax = ecx;
+		eax = eax / 31;
+		
+		edx = edx - eax;
+		
+		a = a + 1;
+	}
+	
+	eax = 0;
+	return;
+}
